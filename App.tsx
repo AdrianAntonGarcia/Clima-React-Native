9170c9c4257e6ba93568f7dab8f13393ebdc290a
adriton <adrtler@gmail.com>
2021-03-21 20:28:44 +0100

    Cambiando color de fondo según la temperatura

--- App.tsx
+++ App.tsx
@@ -15,7 +15,12 @@ const App = () => {
     pais: '',
   });
   const [consultar, setConsultar] = useState(false);
-  const [resultado, setResultado] = useState({});
+  const [resultado, setResultado] = useState({
+    name: '',
+    main: {temp: 0, temp_min: 0, temp_max: 0},
+    weather: [],
+  });
+  const [bgColor, setbgColor] = useState('rgb(71,149,212)');
   const {ciudad, pais} = busqueda;
   useEffect(() => {
     const consultarClima = async () => {
@@ -26,6 +31,18 @@ const App = () => {
           const respuesta = await fetch(url);
           const resultadoApi = await respuesta.json();
           setResultado(resultadoApi);
+          // Modifica los colores del fondo basado en la temperatura
+
+          const kelvin = 273.15;
+          const {main} = resultado;
+          const actual = main.temp - kelvin;
+          if (actual < 10) {
+            setbgColor('rgb(105,108,149)');
+          } else if (actual >= 10 && actual < 25) {
+            setbgColor('rgb(71,149,212)');
+          } else {
+            setbgColor('rgb(178,28,61)');
+          }
         } catch (error) {
           mostrarAlerta();
         }
@@ -33,7 +50,7 @@ const App = () => {
       }
     };
     consultarClima();
-  }, [ciudad, consultar, pais]);
+  }, [ciudad, consultar, pais, resultado]);
   const mostrarAlerta = () => {
     Alert.alert('Error', 'No hay resultados, intenta con otra ciudad o páis', [
       {text: 'Ok'},
@@ -42,10 +59,13 @@ const App = () => {
   const ocultarTeclado = () => {
     Keyboard.dismiss();
   };
+  const bgColorApp = {
+    backgroundColor: bgColor,
+  };
   return (
     <>
       <TouchableWithoutFeedback onPress={() => ocultarTeclado()}>
-        <View style={styles.app}>
+        <View style={[styles.app, bgColorApp]}>
           <View style={styles.contenido}>
             <Clima resultado={resultado} />
             <Formulario
@@ -63,7 +83,6 @@ const App = () => {
 const styles = StyleSheet.create({
   app: {
     flex: 1,
-    backgroundColor: 'rgb(71,149,212)',
     justifyContent: 'center',
   },
   contenido: {
